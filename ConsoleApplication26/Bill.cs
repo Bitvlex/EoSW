1bf9c5373e80696f043c1215e6108bc9c0c077af
Вадим Пустовалов < >
2017-12-22 23:29:00 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication26/Bill.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication26
+{
+    public class Bill
+    {
+        private List<Item> _items;
+        private Customer _customer;
+        public double f;
+        public double bonus;
+        public string result;
+        public Bill(Customer customer)
+        {
+            this._customer = customer;
+            this._items = new List<Item>();
+        }
+        public void addGoods(Item arg)
+        {
+            _items.Add(arg);
+        }
+        public string GetHeader()
+        {
+            string result = "Счет для " + _customer.getName() + "\n" + "\t" + "Название" + "\t" + "Цена" + "\t" + "Кол-во" + "Стоимость" + "\t" + "Скидка" + "\t" + "Сумма" + "\t" + "Бонус" + "\n";
+            return result;
+        }
+        public double GetSum(Item each)
+        {
+            double sum = each.getQuantity() * each.getPrice();
+            return sum;
+        }
+        public string ReturnInfo(double amount, double bonuses)
+        {
+            string info = "Сумма счета составляет " + amount.ToString() + "\n" + "Вы заработали " + bonuses.ToString() + " бонусных балов";
+            return info;
+        }
+        public double UsingBonuses(Item each)
+        {
+            double discount = 0;
+            if ((each.getGoods().getPriceCode() == Goods.REGULAR) && each.getQuantity() > 5)
+            {
+                discount += _customer.useBonus((int)(GetSum(each)));
+            }
+            if ((each.getGoods().getPriceCode() == Goods.SPECIAL_OFFER) && each.getQuantity() > 1)
+            {
+                discount = _customer.useBonus((int)(GetSum(each)));
+            }
+            return discount;
+        }
+        public string Showing_result2(double thisAmount, double discount, double bonus, Item each)
+        {
+            string result = null;
+            result = "\t" + each.getGoods().getTitle() + "\t" + "\t" + each.getPrice() + "\t" + each.getQuantity() + "\t" + (each.getQuantity() * each.getPrice()).ToString() + "\t" + discount.ToString() + "\t" + thisAmount.ToString() + "\t" + bonus.ToString() + "\n";
+            return result;
+        }
+        public int Get_Bonuses(Item each)
+        {
+            int bonuses = 0;
+
+            switch (each.getGoods().getPriceCode())
+            {
+                case Goods.REGULAR:
+                    if (each.getQuantity() > 2)
+                    {
+                        bonuses = (int)(GetSum(each) * 0.05);
+                    }
+                    break;
+                case Goods.SALE:
+                    if (each.getQuantity() > 3)
+                    {
+                        bonuses = (int)(GetSum(each) * 0.01);
+                    }
+                    break;
+            }
+            return bonuses;
+        }
+        public double Get_Discount(Item each)
+        {
+            double discount = 0;
+            switch (each.getGoods().getPriceCode())
+            {
+                case Goods.REGULAR:
+                    if (each.getQuantity() > 2)
+                    {
+                        discount = (GetSum(each)) * 0.03;
+                    }
+                break;
+                case Goods.SPECIAL_OFFER:
+                    if (each.getQuantity() > 10)
+                    {
+                        discount = (GetSum(each)) * 0.005;
+                    }
+                break;
+                case Goods.SALE:
+                    if (each.getQuantity() > 3)
+                    {
+                        discount = (GetSum(each)) * 0.01;
+                    }
+                break;
+            }
+            return discount;
+        }
+        public String statement()
+        {
+            double totalAmount = 0;
+            int totalBonus = 0;
+            List<Item>.Enumerator items = _items.GetEnumerator();
+            result = GetHeader();
+            while (items.MoveNext())
+            {
+                double discount = 0;
+                int bonus = 0;
+                Item each = (Item)items.Current;        
+                discount = Get_Discount(each);
+                bonus = Get_Bonuses(each);                
+                double thisAmount = each.getQuantity() * each.getPrice();
+
+                discount += UsingBonuses(each);
+
+                thisAmount = each.getQuantity() * each.getPrice() - discount;
+
+                result += Showing_result2(thisAmount, discount, bonus, each);
+                totalAmount += thisAmount;
+                totalBonus += bonus;
+            }
+            result = result + ReturnInfo(totalAmount, totalBonus);
+
+            f = Convert.ToDouble(totalAmount);
+            bonus = Convert.ToDouble(totalBonus);
+
+            _customer.receiveBonus(totalBonus);
+            return result;
+        }
+    }
+}
---
+++ TestLibrary/TestClass.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication26;
+using NUnit.Framework;
+
+namespace TestLibrary
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void BaseTest()
+        {
+            Goods fanta = new Goods("Fanta", Goods.REGULAR);
+            Goods snikers = new Goods("Snikers", Goods.SALE);
+            int price_for_fanta = 45;
+            int price_for_snikers = 33;
+            Item i1 = new Item(fanta, 2, price_for_fanta);
+            Item i2 = new Item(snikers, 3, price_for_snikers);
+            Customer Anzor = new Customer("Anzor", 10);
+            Bill b1 = new Bill(Anzor);
+            b1.addGoods(i1);
+            b1.addGoods(i2);
+            string bill = b1.statement();
+            double actual = b1.f; 
+            double exepted = 189;
+            Assert.AreEqual(exepted, actual); 
+        }
+        [Test]
+        public void BaseTest2()
+        {
+            Goods cola = new Goods("Cola", Goods.REGULAR);
+            Goods pepsi = new Goods("Pepsi", Goods.SALE);
+            Item i1 = new Item(cola, 6, 65);
+            Item i2 = new Item(pepsi, 3, 50);
+            Customer x = new Customer("Anzor", 10);
+            Bill b1 = new Bill(x); 
+            b1.addGoods(i1);
+            b1.addGoods(i2);
+            string bill = b1.statement();
+            double actual = b1.f;
+            double exepted = 518.29999999999995;
+            Assert.AreEqual(exepted, actual);
+        }
+        [Test]
+        public void Test_with_three()
+        {
+            Goods pivo = new Goods("Pivo", Goods.REGULAR);
+            Goods chipsi = new Goods("Chipsi", Goods.SALE);
+            Goods nuts = new Goods("Nuts", Goods.SPECIAL_OFFER);
+            Item i1 = new Item(pivo, 2, 67);
+            Item i2 = new Item(chipsi, 4, 45);
+            Item i3 = new Item(nuts, 2, 27);
+            Customer x = new Customer("Anzor", 15);
+            Bill b1 = new Bill(x);
+            b1.addGoods(i1);
+            b1.addGoods(i2);
+            b1.addGoods(i3);
+            string bill = b1.statement();
+            double actual = b1.f;
+            double exepted = 351.19999999999999;
+            Assert.AreEqual(exepted, actual);
+        }
+        [Test]
+        public void BonusBallTest_Base()
+        {
+            Goods cola = new Goods("Cola", Goods.REGULAR);
+            Goods pepsi = new Goods("Pepsi", Goods.SALE);
+            Item i1 = new Item(cola, 6, 65);
+            Item i2 = new Item(pepsi, 3, 50);
+            Customer x = new Customer("Anzor", 10);
+            Bill b1 = new Bill(x);
+            b1.addGoods(i1);
+            b1.addGoods(i2);
+            string bill = b1.statement();
+            double actual = b1.bonus;
+            double exepted = 19;
+            Assert.AreEqual(exepted, actual);
+        }
+        [Test]
+        public void BonusBallTest2_Base()
+        {
+            Goods fanta = new Goods("Fanta", Goods.REGULAR);
+            Goods snikers = new Goods("Snikers", Goods.SALE);
+            int price_for_fanta = 45;
+            int price_for_snikers = 33;
+            Item i1 = new Item(fanta, 2, price_for_fanta);
+            Item i2 = new Item(snikers, 3, price_for_snikers);
+            Customer Anzor = new Customer("Anzor", 10);
+            Bill b1 = new Bill(Anzor);
+            b1.addGoods(i1);
+            b1.addGoods(i2);
+            string bill = b1.statement();
+            double actual = b1.bonus;
+            double exepted = 0;
+            Assert.AreEqual(exepted, actual);
+        }
+    }
+}
